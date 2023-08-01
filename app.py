effe08d405b426a952a6a83291f73897fa7b1919
Javier González Berenguel <jgb@aety.io>
2023-08-01 17:13:20 +0200

    + better mock

--- app.py
+++ app.py
@@ -2,14 +2,20 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-
-@app.route('/echo', methods=['POST', 'PUT'])
+@app.route('/echo', methods=['POST', 'PUT', 'GET'])
 def echo():
+    response = {}
+    if request.args:
+        response["args"] = request.args
+    if request.headers:
+        response["headers"] = dict(request.headers)
+    if request.data:
+        response["data"] = request.data
     if request.is_json:
-        return request.json
+        response["json"] = request.json
     else:
-        return request.form
-
+        response["form"] = request.form
+    return response
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5000)
